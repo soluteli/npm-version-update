17420c37b0e04fe165888d5aee5c5b5acfd46ad3
soluteli <lancetcn@qq.com>
2018-12-25 22:23:28 +0800

    fix: exlude

--- command/get-version.js
+++ command/get-version.js
@@ -1,18 +1,18 @@
 const path = require('path')
 const glob = require('glob')
 const pickVersion = require('./pick-version')
-const util = require('util');
-const globAsync = util.promisify(glob);
+const util = require('util')
+const globAsync = util.promisify(glob)
 
 const basePath = process.cwd()
 let resolve = (dir = '.') => path.posix.join(basePath, dir)
 
 async function getPickList(dir = '.') {
-  let pathToSearch = resolve(dir)
-  let pattern = dir === '.' ? 'package.json' : `${dir}/**!(node_modules)/package.json`
-  let filesArr = await globAsync(pattern, pathToSearch)
-
-  let toPickList = filesArr.map(iii => {
+  let pattern = dir === '.' ? 'package.json' : `${dir}/**/package.json`
+  let filesArr = await globAsync(pattern, {
+    ignore: `**/node_modules/**/package.json`
+  })
+  let toPickList = filesArr.map((iii) => {
     let filePath = resolve(iii)
     return {
       pkg: require(filePath),
@@ -22,11 +22,11 @@ async function getPickList(dir = '.') {
   return toPickList
 }
 
-async function getVersions (dir) {
+async function getVersions(dir) {
   let diffMap = []
   let toPickList = await getPickList(dir)
   for (let item of toPickList) {
-    let {version: cur} = item.pkg
+    let { version: cur } = item.pkg
     item['cur'] = cur
     let ret = await pickVersion(item.pkg)
     item['next'] = ret.version
